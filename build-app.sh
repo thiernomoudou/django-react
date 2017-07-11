4c34e321a10a3e3e5ef12ff7e5ef676d3deb1ac7
thiernomoudou <souleymanemoudou@gmail.com>
2017-07-11 17:48:44 +0000

    fixing the error in the build-app.sh file

--- build-app.sh
+++ build-app.sh
@@ -1,6 +1,6 @@
 # Uncomment the following lines if you want the script to set up your virtualenv for you:
 virtualenv env
-source virtualenv/bin/activate
+source env/bin/activate
 
 # Install dependencies and setup data
 pip install -r requirements.txt
